3ae606116f1154db28e956af280e6378b51f3c13
carlosmart7104 <carlosmart7104@gmail.com>
2018-04-24 04:14:36 -0500

    nuevo sitio temporal: simple countdown

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'static')))
+  .use(express.static(path.join(__dirname, 'public')))
+  //.use(express.static(path.join(__dirname, 'static')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  //.get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
