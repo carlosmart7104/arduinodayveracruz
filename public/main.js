2daa3fe81fcab08a20c2e3d1cd4b948b120b436d
carlos martinez <carlosmart7104@gmail.com>
2019-03-06 19:10:36 -0600

    se actualizo el sitio para el arduino day 2019

---
+++ public/main.js
@@ -0,0 +1,29 @@
+//alert('hola mundo')
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var map = L.map('map').
+setView([19.174034, -96.188779],
+15);
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 18
+}).addTo(map);
+L.control.scale().addTo(map);
+L.marker([41.66, -4.71],{draggable: true}).addTo(map);
+
+
+function mostrar(nombre){
+  if (document.getElementById(nombre).style.display == 'none') {
+    document.getElementById(nombre).style.display = 'block';
+    }
+  else{
+    document.getElementById(nombre).style.display = 'none';
+  }
+  }
